a91e93def38590ad503a04dfb01ef3f57e9886ac
PAULbigBA <paulbigba@outlook.com>
2023-02-28 20:27:48 -0700

    update

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,18 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('signup', views.signup, name='signup'),
-    path('signin', views.signin, name='signin'),
-    path('settings', views.settings, name='settings'),
-    path('search', views.search, name='search'),
-    path('upload', views.upload, name='upload'),
-    path('logout', views.logout, name='logout'),
-    path('profile/<str:pk>', views.profile, name='profile'),
-    path('follow', views.follow, name='follow'),
-    path('like-post', views.like_post, name='like-post'),
-
+    path('admin/', admin.site.urls),
 ]
