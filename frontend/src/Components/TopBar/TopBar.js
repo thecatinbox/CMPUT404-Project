0ffc398f8a2b4877d6dcbff84026ab8f488e6cad
Vivian <catinboxx@gmail.com>
2023-02-28 21:15:04 -0700

    Add views

--- frontend/src/Components/TopBar/TopBar.js
+++ frontend/src/Components/TopBar/TopBar.js
@@ -3,16 +3,16 @@ import "./TopBar.css";
 
 const TopBar = () => {
   return (
-    <div class="topnav">
-      <script src="https://kit.fontawesome.com/68d79784aa.js" crossorigin="anonymous"></script>
-      <a class="active" href="#main">Main</a>
+    <div className="topnav">
+      <script src="https://kit.fontawesome.com/68d79784aa.js" crossOrigin="anonymous"></script>
+      <a className="active" href="#main">Main</a>
       <a href="#inbox">Inbox</a>
       <a href="#friends">Friends</a>
       <a href="#profile">Profile</a>
-      <div class="search-container">
+      <div className="search-container">
         <form action="/action_page.php">
           <input type="text" placeholder="Search.." name="search"></input>
-          <button type="submit"><i class="fa fa-search"></i></button>
+          <button type="submit"><i className="fa fa-search"></i></button>
         </form>
       </div>
     </div>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import TopBar from './Components/TopBar/TopBar'; 
-import App from './App';
+import Home from './Views/Home/Home';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <TopBar />
-    <App />
+    <Home />
   </React.StrictMode>
 );
 
