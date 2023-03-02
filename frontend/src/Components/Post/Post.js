f5951d60fc280fac9beebf4e42a9e57b9c78e2d9
Vivian <catinboxx@gmail.com>
2023-03-01 21:56:22 -0700

    Add edit & delete post

--- frontend/src/Components/Post/Post.js
+++ frontend/src/Components/Post/Post.js
@@ -2,16 +2,65 @@ import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
+import { CardHeader, IconButton } from '@mui/material';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import TextField from '@mui/material/TextField';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart, faChevronDown, faComment, faShare} from '@fortawesome/free-solid-svg-icons'
+
 import "./Post.css"; 
 
 function Post({post}) { 
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <div className='post'>
       <Card sx={{ minWidth: 275 }}>
+        <CardActions disableSpacing
+          sx={{
+            alignSelf: "stretch",
+            display: "flex",
+            justifyContent: "flex-end",
+            alignItems: "flex-start",
+            p: 0,
+          }}>
+          <div>
+            <Button
+              id="basic-button"
+              aria-controls={open ? 'basic-menu' : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? 'true' : undefined}
+              onClick={handleClick}
+            >
+              <FontAwesomeIcon icon={faChevronDown} />
+            </Button>
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                'aria-labelledby': 'basic-button',
+              }}
+            >
+              <MenuItem onClick={handleClose}>Edit Post</MenuItem>
+              <MenuItem onClick={handleClose}>Delete Post</MenuItem>
+            </Menu>
+          </div>
+        </CardActions>
+        
         <CardContent>
           <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
             {post.author}
@@ -26,10 +75,17 @@ function Post({post}) {
             {post.content}
           </Typography>
         </CardContent>
-        <CardActions>
-          <Button size="small">Like</Button>
-          <Button size="small">Comment</Button>
-          <Box sx={{ m: 1}} /> 
+
+        <CardActions disableSpacing>
+          <IconButton>
+            <FontAwesomeIcon icon={faHeart} />
+          </IconButton>
+          <IconButton>
+            <FontAwesomeIcon icon={faComment} />
+          </IconButton>
+          <IconButton className>
+            <FontAwesomeIcon icon={faShare} />
+          </IconButton>
           <TextField hiddenLabel id="comment-text" size="small" label="Comment" variant="outlined" />
           <Button size="small">Send</Button>
         </CardActions>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useLocation } from 'react-router-dom'
 import TopBar from "./Components/TopBar/TopBar";
 import Home from "./Pages/Home/Home";
 import Inbox from "./Pages/Inbox/Inbox";
