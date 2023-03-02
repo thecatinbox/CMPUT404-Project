c855b84e496a5972b0f8df61a38a0e8410b66e0d
Vivian <catinboxx@gmail.com>
2023-03-01 20:23:33 -0700

    Revert "Merge pull request #4 from thecatinbox/MingYe"
    This reverts commit 86f2f5ddf1f3fea3a42f35562ff0ff641120556d, reversing
    changes made to 996d8f62d997038d791fc4bb2126f881bdb5756a.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
-import TopBar from './Components/TopBar/TopBar'; 
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <TopBar />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
