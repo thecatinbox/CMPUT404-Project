e34b35208c97bd25c3ac896202541f2d739ee2bf
PAULbigBA <paulbigba@outlook.com>
2023-03-04 14:14:21 -0700

    view upload

---
+++ backend/signIn/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
+from allModels.models import *
+from django.urls import reverse
+
+
+def signIn(request):
+    # login by username and password
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            userLogin = Authors.objects.get(username=username)
+        except:
+            return HttpResponse("Username does not exist")
+
+        if userLogin.is_active == True:
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                current_user = request.user
+                current_author_id = Authors.objects.get(username=current_user).uuid
+                # url should be main/authorId
+                return HttpResponseRedirect(reverse("main", args=[current_author_id]))
+            else:
+                return HttpResponse("The username or password are incorrect.")
+
+        else:
+            return HttpResponse("Account is not active")
+    else:
+        return render(request, "signIn.html", {'error': 'Invalid login credentials'})
