9b557f256816cc2ff8090f3b32f0f22025d0657b
PAULbigBA <paulbigba@outlook.com>
2023-03-04 13:56:30 -0700

    update

--- backend/signIn/views.py
+++ backend/signIn/views.py
@@ -5,33 +5,32 @@ from django.http import HttpResponseRedirect
 from allModels.models import *
 from django.urls import reverse
 
+
 def signIn(request):
-    #login by username and password
+    # login by username and password
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
 
         try:
-            userLogin = Authors.objects.get(username = username)
+            userLogin = Authors.objects.get(username=username)
         except:
             return HttpResponse("Username does not exist")
 
         if userLogin.is_active == True:
-            
+
             user = authenticate(request, username=username, password=password)
 
             if user is not None:
-                login(request,user)
+                login(request, user)
                 current_user = request.user
-                current_author_id = single_author.objects.get(username=current_user).uuid
-                #url should be main/authorId
-                return HttpResponseRedirect(reverse("main",args=[current_authorId]))
+                current_author_id = Authors.objects.get(username=current_user).uuid
+                # url should be main/authorId
+                return HttpResponseRedirect(reverse("main", args=[current_author_id]))
             else:
                 return HttpResponse("The username or password are incorrect.")
- 
+
         else:
             return HttpResponse("Account is not active")
     else:
-        return render(request,"signIn.html",{'error': 'Invalid login credentials'})
-
-
+        return render(request, "signIn.html", {'error': 'Invalid login credentials'})
