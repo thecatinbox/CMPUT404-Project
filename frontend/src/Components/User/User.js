ce2169e4e6022674fc2f54dca31cdde7467b723b
Vivian <catinboxx@gmail.com>
2023-03-18 17:05:21 -0600

    Search user functionality

--- frontend/src/Components/User/User.js
+++ frontend/src/Components/User/User.js
@@ -3,20 +3,27 @@ import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 import "./User.css"; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons'
 
 function User({user}) { 
 
   return (
     <div className='user'>
       <Card sx={{ minWidth: 275 }}>
-        <CardContent>
-          <Typography>
-            {user.displayName}
-          </Typography>
-        </CardContent>
-
+      <CardContent sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Typography sx={{ alignSelf: 'flex-start' }}>
+          {user.displayName}
+        </Typography>
+        <Typography sx={{ alignSelf: 'flex-end' }}>
+          <Button>
+            <FontAwesomeIcon icon={faUserPlus} />
+          </Button>
+        </Typography>
+      </CardContent>
       </Card>
     </div>
   );
