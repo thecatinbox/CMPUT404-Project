a6e3d5d6cd3752f7ca056163299db8086c4e712d
Vivian <catinboxx@gmail.com>
2023-03-20 17:29:32 -0600

    Update Comment.js

--- frontend/src/Components/Comment/Comment.js
+++ frontend/src/Components/Comment/Comment.js
@@ -8,28 +8,15 @@ import { faHeart } from '@fortawesome/free-solid-svg-icons'
 import "./Comment.css";
 
 function Comment({comment}) { 
-
-    // const [inputs, setInputs] = useState({});
-    // const handleChange = (event) => {
-    //     const { name, value } = event.target;
-    //     setInputs((prevInputs) => ({ ...prevInputs, [name]: value }));
-    //     console.log(inputs); 
-    // };
-    // const userName = "123";
-    // const userComment = "hello";
-    // const likeNum = 10;
-
     return (
         <div className="comment">
             <IconButton className="likeComment" > 
                 <FontAwesomeIcon icon={faHeart} />
             </IconButton>
             <div className="commentText">
-                <h4 className="userName" > user </h4>
-                <div className="userComment" > Hello World </div>
+                <h4 className="userName" > {comment.author.displayName} </h4>
+                <div className="userComment" > {comment.comment} </div>
             </div>
-            
-            
         </div>
     );
 }
