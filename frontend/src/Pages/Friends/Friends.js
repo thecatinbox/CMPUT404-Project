e0ea3065de04dc1208e2bb4fb667ac35fdd0e745
Vivian <catinboxx@gmail.com>
2023-03-20 10:14:18 -0600

    Add follower fetch

--- frontend/src/Pages/Friends/Friends.js
+++ frontend/src/Pages/Friends/Friends.js
@@ -1,7 +1,9 @@
 import User from '../../Components/User/User'; 
 import TopBar from "../../Components/TopBar/TopBar";
 import './Friends.css';
+import React, { useState, useEffect } from "react";
 
+/* 
 const followingData = [
   {
     id: 1, 
@@ -35,8 +37,47 @@ const followerData = [
     displayName: "Username6"
   },
 ];
+*/ 
 
 function Friends() {
+
+  const uuid = localStorage.getItem('uuid'); 
+  const app_url = localStorage.getItem('url'); 
+
+  var FOLLOWER_ENDPOINT = "http://" + app_url + "/server/authors/" + uuid + "/followers/"; 
+  var FOLLOWING_ENDPOINT = "http://" + app_url + "/server/authors/" + uuid + "/followers/"; 
+
+  const [followerData, setFollowerData] = useState([]);
+  const [followingData, setFollowingData] = useState([]);
+
+  function fetchData() {
+    try {
+      fetch(FOLLOWER_ENDPOINT, {
+        headers: { "Accept": "application/json" },
+        method: "GET"
+      }).then(response => response.json()).then(data => {
+        setFollowerData(data.items);
+      });
+    } catch (error) {
+      console.error('Error:', error);
+    }
+
+    try {
+      fetch(FOLLOWING_ENDPOINT, {
+        headers: { "Accept": "application/json" },
+        method: "GET"
+      }).then(response => response.json()).then(data => {
+        setFollowingData(data.items);
+      });
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }); 
+
   return (
     <>
       <TopBar id="friends"/>
