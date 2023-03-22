de180c6bd574427ffa03c9105a433c05b3a99784
Vivian <catinboxx@gmail.com>
2023-03-21 21:35:15 -0600

    Add auto navigation if not signed in

--- frontend/src/Pages/Home/Home.js
+++ frontend/src/Pages/Home/Home.js
@@ -2,16 +2,25 @@ import React, {useState, useEffect} from 'react';
 import Post from '../../Components/Post/Post'; 
 import AddPost from '../../Components/AddPost/AddPost'; 
 import TopBar from "../../Components/TopBar/TopBar";
+import { useNavigate } from "react-router-dom";
 import './Home.css';
 
 function Home() {
 
   // const uuid = localStorage.getItem('uuid'); 
   // console.log(uuid); 
+  const navigate = useNavigate();
+  const [postList, setPostList] = useState([]);
+
+  if (!localStorage.getItem('uuid')) {
+    navigate("/signin");
+  }
+
+  const uuid = localStorage.getItem('uuid'); 
+  // console.log(uuid); 
+
   const app_url = localStorage.getItem('url'); 
   const ENDPOINT = 'http://' + app_url + '/server/posts/'; 
-
-  const [postList, setPostList] = useState([]);
 
   async function fetchData() {
     try {
