c7c6769af8017bf64ed99b43aaa38a526c820168
JiaYiChe <59781205+JiaYiChe@users.noreply.github.com>
2023-03-24 12:54:56 -0600

    add more host and white list

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,10 +27,10 @@ SECRET_KEY = 'django-insecure-$%q1-_ii&csvh^so0i0m8x=1%-x-+l74*ks5q2km*tjexv8%gd
 DEBUG = True
 
 
-ALLOWED_HOSTS = ["cmput404-project-app.herokuapp.com","cmput404-project-data.herokuapp.com",'localhost','127.0.0.1']
+ALLOWED_HOSTS = ["cmput404-project-app.herokuapp.com","cmput404-project-data.herokuapp.com",'localhost','127.0.0.1','sd16-api.herokuapp.com','peer2pressure.herokuapp.com']
 
 CORS_ALLOWED_CREDENTIALS = True
-CORS_ORIGIN_WHITELIST = ['http://localhost:3000', 'https://cmput404-project-app.herokuapp.com', 'https://cmput404-project-data.herokuapp.com','http://localhost:8000','http://127.0.0.1:3000','http://127.0.0.1:8000',]
+CORS_ORIGIN_WHITELIST = ['http://localhost:3000', 'https://cmput404-project-app.herokuapp.com', 'https://cmput404-project-data.herokuapp.com','http://localhost:8000','http://127.0.0.1:3000','http://127.0.0.1:8000','http://sd16-api.herokuapp.com','https://peer2pressure.herokuapp.com',' https://sd16-api.herokuapp.com']
 
 CORS_ALLOW_METHODS = (
     'DELETE',
