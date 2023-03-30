1ddcd296d94c3cc5a4a8173626dbd5fe430ce930
PAULbigBA <paulbigba@outlook.com>
2023-03-29 19:59:07 -0600

    remove unuse url and add github url

--- backend/authors/urls.py
+++ backend/authors/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path("authors/<str:userId>/search", views.search, name="search_users"),
-    path("logout", views.logout, name='author_logout'),
 ]
--- backend/service/urls.py
+++ backend/service/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('posts/',views.getAllPublicPosts,name="get_All_Public_Posts"),
     path('authors/',views.authorsList, name="authors_List"),
     path('authors/<str:pk>/',views.singleAuthor,name="single_Author"),
+    path('authors/<str:pk>/github',views.showGithubActivity,name="github_Activity"),
     path('authors/<str:pk>/posts/',views.Post,name="ones_Posts"),
     path('authors/<str:pk>/posts/<str:postsId>/',views.get_post,name="get_Posts"),
     path('authors/<str:pk>/posts/<str:postsId>/comments',views.getComments,name="get_Comments"),
@@ -19,4 +20,5 @@ urlpatterns = [
     path('authors/<str:pk>/comments/liked',views.get_liked_comments,name="get_LikedComments"),
     path('authors/<str:pk>/comments/<str:commentId>/likes',views.get_comment_likes,name="get_Comments Likes"),
     path('authors/<str:pk>/inbox',views.inbox,name="ones_Inbox"),
+
 ]
